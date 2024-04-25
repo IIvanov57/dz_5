a48a919c97ccc6677aa5c1d10bf0fda85a23f446
IIvanov57 <ivanov080892@mail.ru>
2024-04-25 15:05:50 +0300

    add test checkEnterprisePage

--- src/test/java/SolutionsTests.java
+++ src/test/java/SolutionsTests.java
@@ -1,2 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SolutionsTests {
+
+
+  @Test
+  void checkEnterprisePage(){
+
+    open("https://github.com/");
+    $("ul > li:nth-child(2) > button").hover();
+    sleep(3000);
+    $("a[href='/enterprise']").click();
+
+    $("#hero-section-brand-heading").shouldHave(text("The AI-powered\n" +
+            "developer platform."));
+
+
+  }
+
 }
